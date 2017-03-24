089bd476d333a1e840e2994dd483a06aa8fc2493
flopeters1337 <fl0wryan@hotmail.com>
2017-03-25 00:40:54 +0100

    Add bot logic

--- gifio-bot/__init__.py
+++ gifio-bot/__init__.py
@@ -1,7 +1,1 @@
-import twitter
-
-                               # Change those with your own API keys and access tokens.
-api = twitter.Api(consumer_key='<consumer key>',
-                  consumer_secret='<consumer secret>',
-                  access_token_key='<access token key>',
-                  access_token_secret='<access token secret>')
+ 
