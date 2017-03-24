f1f411b234a1e386de1487f3c85396c56f059bf3
Peters Florian <florian.peters@student.ulg.ac.be>
2017-03-24 21:53:04 +0100

    Add comments to __init__.py

--- gifio-bot/__init__.py
+++ gifio-bot/__init__.py
@@ -1,5 +1,6 @@
 import twitter
 
+                               # Change those with your own API keys and access tokens.
 api = twitter.Api(consumer_key='<consumer key>',
                   consumer_secret='<consumer secret>',
                   access_token_key='<access token key>',
